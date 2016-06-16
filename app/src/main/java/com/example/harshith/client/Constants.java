b0036a2a9ecc3c3da297ab16fe80d056a6ec0d58
Harshith Goka <harshith9399@gmail.com>
2016-06-16 22:40:08 +0530

    nothing

---
+++ app/src/main/java/com/example/harshith/client/Constants.java
@@ -0,0 +1,10 @@
+package com.example.harshith.client;
+
+/**
+ * Created by harshith on 15/6/16.
+ */
+
+public class Constants {
+    public static final String BROADCAST_ACTION = "com.example.harshith.client.BROADCAST";
+    public static final String EXTENDED_DATA_STATUS = "com.example.harshith.client.STATUS";
+}
